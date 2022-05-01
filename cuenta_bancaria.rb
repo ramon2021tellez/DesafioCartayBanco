e3f0193fd5d8a20cac1b9e8e79baba6425e0dbe0
ramon2021tellez <101756322+ramon2021tellez@users.noreply.github.com>
2022-05-01 15:07:10 -0400

    five commit

--- cuenta_bancaria.rb
+++ cuenta_bancaria.rb
@@ -1,17 +1,10 @@
-#Ejercicio 2
-#Crear clase CuentaBancaria
 class CuentaBancaria
-    #getter y setter para el atributo nombre_usuario
     attr_accessor :nombre_usuario
-    #solo lectura para atributo vip y numero de cuenta
     attr_reader :vip, :numero_de_cuenta
-    #metodo constructor
     def initialize(nombre_usuario, numero_de_cuenta, vip = 0)
-        if numero_cuenta.digits.count != 8
+        if numero_de_cuenta.digits.count != 8
             raise RangeError, "Número de dígitos incorrecto"
-        end
-
-        if !(0..1).include? (vip)
+        elsif !(0..1).include? (vip)
             raise RangeError, "Número VIP incorrecto"
         end
         @nombre_usuario = nombre_usuario
@@ -19,11 +12,11 @@ class CuentaBancaria
         @vip = vip
     end
     def cuenta_vip
-        vip-numero_de_cuenta
+        "#{vip}-#{numero_de_cuenta}"
     end
 end
-#se instancia la variable resultado
-resultado = CuentaBancaria.new("marcos", 87654321, 1)
-#se muestra resultado nombre usuario y cuenta vip 
-puts resultado.nombre_usuario
-#puts resultado.cuenta_vip
+
+results = CuentaBancaria.new("marcos", 87654321, 1)
+
+puts results.nombre_usuario
+puts results.cuenta_vip
