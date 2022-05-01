0669601905cd411bbf7686464b9ff34cc0c56611
ramon2021tellez <101756322+ramon2021tellez@users.noreply.github.com>
2022-05-01 14:14:11 -0400

    four commit

--- cuenta_bancaria.rb
+++ cuenta_bancaria.rb
@@ -19,11 +19,11 @@ class CuentaBancaria
         @vip = vip
     end
     def cuenta_vip
-        "#{self.vip}-#{self.numero_de_cuenta}"
+        vip-numero_de_cuenta
     end
 end
 #se instancia la variable resultado
 resultado = CuentaBancaria.new("marcos", 87654321, 1)
 #se muestra resultado nombre usuario y cuenta vip 
 puts resultado.nombre_usuario
-puts resultado.cuenta_vip
+#puts resultado.cuenta_vip
